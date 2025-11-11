b7c57e6bd776a721a867f2c7fa5405c5d244eb3f
unknown <roberthbardales@hotmail.com>
2025-11-10 21:04:32 -0500

    hora de envio online/offline

--- chat_project/chat/models.py
+++ chat_project/chat/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils import timezone
+
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
@@ -13,3 +16,15 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.sender} -> {self.recipient}: {self.content[:20]}'
+
+
+
+class UserStatus(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_online = models.BooleanField(default=False)
+    last_seen = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {'Online' if self.is_online else 'Offline'}"
+
+
--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -41,6 +41,7 @@ class LogoutView(View):
         logout(request)
         return redirect('login')
 
+from django.http import JsonResponse
 
 class ChatHomeView(LoginRequiredMixin, ListView):
     model = User
@@ -49,8 +50,25 @@ class ChatHomeView(LoginRequiredMixin, ListView):
     login_url = '/login/'
 
     def get_queryset(self):
-        return User.objects.exclude(id=self.request.user.id)
+        users = User.objects.exclude(id=self.request.user.id)
+        from .models import UserStatus
+        status_map = {s.user_id: s.is_online for s in UserStatus.objects.all()}
+        for user in users:
+            user.is_online = status_map.get(user.id, False)
+        return users
 
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.GET.get('json') == '1':
+            data = {user.id: user.is_online for user in context['users']}
+            return JsonResponse(data)
+        return super().render_to_response(context, **response_kwargs)
+
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from .models import Message
 
 class ChatRoomView(LoginRequiredMixin, TemplateView):
     template_name = 'chat/room.html'
@@ -66,6 +84,38 @@ class ChatRoomView(LoginRequiredMixin, TemplateView):
             recipient__in=[self.request.user, other_user]
         ).order_by('timestamp')
 
+        # Formateamos la hora de cada mensaje
+        messages_with_time = []
+        for msg in messages:
+            messages_with_time.append({
+                'sender': msg.sender,
+                'content': msg.content,
+                'formatted_time': msg.timestamp.strftime('%H:%M')
+            })
+
         context['other_user'] = other_user
-        context['messages'] = messages
+        context['messages'] = messages_with_time
         return context
+
+
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+from .models import UserStatus
+from django.utils import timezone
+
+@receiver(user_logged_in)
+def set_user_online(sender, user, request, **kwargs):
+    status, created = UserStatus.objects.get_or_create(user=user)
+    status.is_online = True
+    status.last_seen = timezone.now()
+    status.save()
+
+@receiver(user_logged_out)
+def set_user_offline(sender, user, request, **kwargs):
+    try:
+        status = UserStatus.objects.get(user=user)
+        status.is_online = False
+        status.last_seen = timezone.now()
+        status.save()
+    except UserStatus.DoesNotExist:
+        pass
